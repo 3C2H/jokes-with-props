6bd6005fb3c130aee6b23ce206fc35a4cbe15777
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-03-02 18:19:48 +0100

    add basic logic if question field empty

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -3,6 +3,14 @@ import '../style/joke.css'
 
 class Joke extends React.Component {
   render() {
+    if (this.props.question === '') {
+      return(
+        <div className='jokeContainer'>
+          <h3 className='punchLine' style={{ marginTop:0 }} > {this.props.punchLine} </h3>
+        </div>
+      )
+    }
+    else
     return(
       <div className='jokeContainer'>
         <h2 className='question'> {this.props.question} </h2> <br></br>
