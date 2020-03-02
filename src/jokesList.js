dca9b962d430522bbef4d14d47048c0ea75a8a25
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-03-02 17:45:23 +0100

    added jokes lines + styling

--- src/jokesList.js
+++ src/jokesList.js
@@ -1,24 +1,26 @@
 module.exports = {
   
   joke1 : {
-    question : 'question 1',
-    punchLine : 'punchline 1',
+    question : 'What do you call it when two rock guitars accidentally crash into each other?',
+    
+    punchLine : 'A Fender bender',
   },
   joke2 : {
-    question : 'question 2',
-    punchLine : 'punchline 2',
+    question : 'What is it called when a snowman has a temper tantrum?',
+    punchLine : 'A meltdown!',
   },
   joke3 : {
-    question : '',
-    punchLine : '',
+    question : 'Why did the teddy bear say no to dessert?',
+    
+    punchLine : 'Because she was stuffed',
   },
   joke4 : {
     question : '',
-    punchLine : '',
+    punchLine : 'RIP boiling water—you will be mist',
   },
   joke5 : {
     question : '',
-    punchLine : '',
+    punchLine : 'I asked my French friend if she likes to play video games. She said, “Wii.”',
   }
 }
 
