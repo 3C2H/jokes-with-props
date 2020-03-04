0df5eb66224b5b4f1b7e9d631488eab6eccbabeb
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-03-04 16:03:29 +0100

    typo + comments on aray use

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import JokesList from './jokesList'
 import Title from './components/Title'
 
 
-// const mappedJokes = jokesList.map(joke => <jokesList key={joke.id} question={joke.question} punchline={joke.punchline} /> )
-
 class App extends React.Component {
 	render() {
 		
 		const mappedJokes = JokesList.map(joke => <Joke key={joke.id} question={joke.question} punchline={joke.punchline} /> )
-				
+			//Use map on JokesList array: each entry named joke returns a *J*oke component with key, question & punchline props		
+		
 		return (
 			<div className="App">
 				<Title />
-				{mappedJokes}
+				{mappedJokes} 
 			</div>
+			//return the new array named mappedJokes
 		)
 	}
 }
 
+// *Old rendering using function keyword*
 // function App() {
 //   return (
 //     <div className="App">
